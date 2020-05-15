210561d509d7ac3d0b05c595cc338efcec5abc97
Kasulejoseph <kasule06joseph@gmail.com>
2020-05-15 13:13:24 +0300

    news feeds

--- src/views/NewFeedsView.vue
+++ src/views/NewFeedsView.vue
@@ -1,48 +1,41 @@
 <template>
   <div>
     <NewFeeds
-    :username="username"
-    :usernameIcon="usernameIcon"
-    :media="media"
-    :statitics="statitics"
-    :details="details"
-    :duration="duration"
-    />
-    <NewFeeds
-    class="mt-4"
-    :username="username"
-    :usernameIcon="usernameIcon"
-    :media="media"
-    :statitics="statitics"
-    :details="details"
-    :duration="duration"
+      class="mb-4"
+      v-for="data in feeds"
+      :key="data.id"
+      :username="username"
+      :usernameIcon="usernameIcon"
+      :media="media"
+      :statitics="statitics"
+      :details="details"
+      :duration="duration"
     />
   </div>
 </template>
 
 <script>
-import NewFeeds from '../components/NewFeeds'
-import video from '../assets/video.png'
+import NewFeeds from "../components/NewFeeds";
+import video from "../assets/video.png";
 
-  export default {
-    name: 'NewFeedsView',
-    components: {
-      NewFeeds
-    },
-    setup() {
-      return {
-        username: 'kawowosports',
-        usernameIcon: '<i class="far fa-user-circle fa-2x"></i>',
-        media: video,
-        statitics: '5 views',
-        details: 'This was the original FIFA/PES.',
-        duration: '1 day'
-      }
-    }
-    
+export default {
+  name: "NewFeedsView",
+  components: {
+    NewFeeds
+  },
+  setup() {
+    const feeds = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    return {
+      username: "kawowosports",
+      usernameIcon: '<i class="far fa-user-circle fa-2x"></i>',
+      media: video,
+      statitics: "5 views",
+      details: "This was the original FIFA/PES.",
+      duration: "1 day",
+      feeds
+    };
   }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
